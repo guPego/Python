fe9ff49eb8fcdf5db095a53e9d39927b43124972
guPego <gustavoprg04@gmail.com>
2022-10-27 20:08:01 -0300

    Atividade 3 27.10.2022

---
+++ Exercicio_Tres_(27.10.22)/Turno.py
@@ -0,0 +1,12 @@
+print("Informe a seguir o turno em que estuda")
+turno = input("M para matutino, V para vespertino e N para noturno: ")
+
+match turno:
+    case "M" | "m":
+        print("Bom dia")
+    case "V" | "v":
+        print("Boa tarde!")
+    case "N" | "n":
+        print("Boa noite!")
+    case _:
+        print("Valor digitado inválido.")

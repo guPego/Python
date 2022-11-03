92099bbe37177b96bb7920a93e76ae9d9a6dc227
guPego <gustavoprg04@gmail.com>
2022-11-03 19:29:18 -0300

    Pirâmide de números e "n"

--- Exercicio_Seis_(01.11.2022)/PiramideImpressa.py
+++ Exercicio_Seis_(01.11.2022)/PiramideImpressa.py
@@ -1,9 +1,19 @@
 print("Pirâmide impressa", "\n")
 
+num = int(input("Digite um número para finalizar a piramide: "))
+print("")
+
 for i in range(4):
     for j in range(i+1):
         if(i < 3):
             print(i+1, end=" ")
         else:
             print("n", end=" ")
+    print("")
+
+print("")
+
+for i in range(0, num):
+    for j in range(i+1):
+        print(i+1, end=" ")
     print("")
